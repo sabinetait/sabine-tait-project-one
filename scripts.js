b81aacf53106453559e436e86e6c9d662d5bea6e
Sabine Tait <sabine.tait@gmail.com>
2021-12-14 09:41:34 -0500

    pseudo code for feature :eyes:

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Add a toggle to the hamburger menu in smaller media queries
+
+// Target the menu icon and save it in a variable
+
+// Target nav ul element to manipulate
+
+// Create an event listener to capture clicks on the menu icon
+
+// Create menu toggle
+// When menu icon is clicked, nav uls are visible
+// When menu icon is clicked again, nav uls are hidden/collapsed
