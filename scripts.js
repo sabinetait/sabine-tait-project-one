c25dcc625ef33eab63d8bff5bfeba13ed4a2fc05
Sabine Tait <sabine.tait@gmail.com>
2021-12-14 21:33:13 -0500

    created toggle for nav menu + linked scripts to blog and contact pages :dancers:

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
 // Add a toggle to the hamburger menu in smaller media queries
 
 // Target the menu icon and save it in a variable
+const menuElement = document.querySelector('.menu');
 
 // Target nav ul element to manipulate
+const navUlElement = document.querySelector('.hide');
 
 // Create an event listener to capture clicks on the menu icon
-
+menuElement.addEventListener('click', function(event) {
+    navToggle(event.target);
+    
+})
 // Create menu toggle
 // When menu icon is clicked, nav uls are visible
 // When menu icon is clicked again, nav uls are hidden/collapsed
+function navToggle() {
+navUlElement.classList.toggle("show");
+}
+
