c784913f87e5e06499ea722a129c85ea748f84bb
Sabine Tait <sabine.tait@gmail.com>
2021-12-15 20:59:28 -0500

    small pseudocode change in js file :pencil2:

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 // Add a toggle to the hamburger menu in smaller media queries
-// Target the menu icon and save it in a variable
+// Capture the hambburger menu icon and save it in a variable
 const menuElement = document.querySelector('.menu');
 
-// Target nav ul element to manipulate
+// Capture mobile nav ul element and save it in a varible
 const navUlElement = document.querySelector('.mobile-only');
 console.log(navUlElement);
 
-// Create an event listener to capture clicks on the menu icon
+// Create an event listener to capture clicks on the hamburger menu icon
 menuElement.addEventListener('click', function(event) {
     navToggle(event.target);
     
 })
-// Create menu toggle
-// When menu icon is clicked, nav uls are visible
-// When menu icon is clicked again, nav uls are hidden/collapsed
+// Create hamburger menu toggle
+// When hamburger menu icon is clicked, mobile nav uls are visible
+// When menu icon is clicked again, hamburger menu nav uls are hidden/collapsed
 function navToggle() {
 navUlElement.classList.toggle("show");
 }
